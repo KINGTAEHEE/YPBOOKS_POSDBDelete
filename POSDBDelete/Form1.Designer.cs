9d443ec125387be4fe166533fa9ccb00a201b805
KINGTAEHEE <thk919@gmail.com>
2020-12-28 11:31:00 +0900

    프로젝트 업로드
    Visual Studio 2015 프로젝트 업로드

---
+++ POSDBDelete/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace POSDBDelete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbBRCODE = new System.Windows.Forms.Label();
+            this.lbWKDATE = new System.Windows.Forms.Label();
+            this.lbPOSNO = new System.Windows.Forms.Label();
+            this.lbGROUPNO = new System.Windows.Forms.Label();
+            this.lbVNO = new System.Windows.Forms.Label();
+            this.tbBRCODE = new System.Windows.Forms.TextBox();
+            this.tbWKDATE = new System.Windows.Forms.TextBox();
+            this.tbPOSNO = new System.Windows.Forms.TextBox();
+            this.tbGROUPNO = new System.Windows.Forms.TextBox();
+            this.tbVNO = new System.Windows.Forms.TextBox();
+            this.btDELETE = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // lbBRCODE
+            // 
+            this.lbBRCODE.AutoSize = true;
+            this.lbBRCODE.Location = new System.Drawing.Point(56, 49);
+            this.lbBRCODE.Name = "lbBRCODE";
+            this.lbBRCODE.Size = new System.Drawing.Size(55, 12);
+            this.lbBRCODE.TabIndex = 0;
+            this.lbBRCODE.Text = "BRCODE";
+            // 
+            // lbWKDATE
+            // 
+            this.lbWKDATE.AutoSize = true;
+            this.lbWKDATE.Location = new System.Drawing.Point(56, 78);
+            this.lbWKDATE.Name = "lbWKDATE";
+            this.lbWKDATE.Size = new System.Drawing.Size(55, 12);
+            this.lbWKDATE.TabIndex = 1;
+            this.lbWKDATE.Text = "WKDATE";
+            // 
+            // lbPOSNO
+            // 
+            this.lbPOSNO.AutoSize = true;
+            this.lbPOSNO.Location = new System.Drawing.Point(63, 105);
+            this.lbPOSNO.Name = "lbPOSNO";
+            this.lbPOSNO.Size = new System.Drawing.Size(48, 12);
+            this.lbPOSNO.TabIndex = 2;
+            this.lbPOSNO.Text = "POSNO";
+            // 
+            // lbGROUPNO
+            // 
+            this.lbGROUPNO.AutoSize = true;
+            this.lbGROUPNO.Location = new System.Drawing.Point(46, 132);
+            this.lbGROUPNO.Name = "lbGROUPNO";
+            this.lbGROUPNO.Size = new System.Drawing.Size(65, 12);
+            this.lbGROUPNO.TabIndex = 3;
+            this.lbGROUPNO.Text = "GROUPNO";
+            // 
+            // lbVNO
+            // 
+            this.lbVNO.AutoSize = true;
+            this.lbVNO.Location = new System.Drawing.Point(80, 159);
+            this.lbVNO.Name = "lbVNO";
+            this.lbVNO.Size = new System.Drawing.Size(31, 12);
+            this.lbVNO.TabIndex = 4;
+            this.lbVNO.Text = "VNO";
+            // 
+            // tbBRCODE
+            // 
+            this.tbBRCODE.Location = new System.Drawing.Point(117, 45);
+            this.tbBRCODE.Name = "tbBRCODE";
+            this.tbBRCODE.Size = new System.Drawing.Size(100, 21);
+            this.tbBRCODE.TabIndex = 5;
+            // 
+            // tbWKDATE
+            // 
+            this.tbWKDATE.Location = new System.Drawing.Point(117, 74);
+            this.tbWKDATE.Name = "tbWKDATE";
+            this.tbWKDATE.Size = new System.Drawing.Size(100, 21);
+            this.tbWKDATE.TabIndex = 6;
+            // 
+            // tbPOSNO
+            // 
+            this.tbPOSNO.Location = new System.Drawing.Point(117, 101);
+            this.tbPOSNO.Name = "tbPOSNO";
+            this.tbPOSNO.Size = new System.Drawing.Size(100, 21);
+            this.tbPOSNO.TabIndex = 7;
+            // 
+            // tbGROUPNO
+            // 
+            this.tbGROUPNO.Location = new System.Drawing.Point(117, 128);
+            this.tbGROUPNO.Name = "tbGROUPNO";
+            this.tbGROUPNO.Size = new System.Drawing.Size(100, 21);
+            this.tbGROUPNO.TabIndex = 8;
+            // 
+            // tbVNO
+            // 
+            this.tbVNO.Location = new System.Drawing.Point(117, 155);
+            this.tbVNO.Name = "tbVNO";
+            this.tbVNO.Size = new System.Drawing.Size(100, 21);
+            this.tbVNO.TabIndex = 9;
+            // 
+            // btDELETE
+            // 
+            this.btDELETE.Location = new System.Drawing.Point(80, 199);
+            this.btDELETE.Name = "btDELETE";
+            this.btDELETE.Size = new System.Drawing.Size(124, 51);
+            this.btDELETE.TabIndex = 10;
+            this.btDELETE.Text = "DELETE";
+            this.btDELETE.UseVisualStyleBackColor = true;
+            this.btDELETE.Click += new System.EventHandler(this.btDELETE_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "※ 신중하게 입력할 것!";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(28, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 163);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "조건";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btDELETE);
+            this.Controls.Add(this.tbVNO);
+            this.Controls.Add(this.tbGROUPNO);
+            this.Controls.Add(this.tbPOSNO);
+            this.Controls.Add(this.tbWKDATE);
+            this.Controls.Add(this.tbBRCODE);
+            this.Controls.Add(this.lbVNO);
+            this.Controls.Add(this.lbGROUPNO);
+            this.Controls.Add(this.lbPOSNO);
+            this.Controls.Add(this.lbWKDATE);
+            this.Controls.Add(this.lbBRCODE);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "POS DB DELETE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbBRCODE;
+        private System.Windows.Forms.Label lbWKDATE;
+        private System.Windows.Forms.Label lbPOSNO;
+        private System.Windows.Forms.Label lbGROUPNO;
+        private System.Windows.Forms.Label lbVNO;
+        private System.Windows.Forms.TextBox tbBRCODE;
+        private System.Windows.Forms.TextBox tbWKDATE;
+        private System.Windows.Forms.TextBox tbPOSNO;
+        private System.Windows.Forms.TextBox tbGROUPNO;
+        private System.Windows.Forms.TextBox tbVNO;
+        private System.Windows.Forms.Button btDELETE;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ POSDBDelete/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSDBDelete
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DBDELETE(string _BRCODE, string _WKDATE, string _POSNO, string _GROUPNO, string _VNO)
+        {
+            // ---> DB 연결하기
+            SqlConnection conn = new SqlConnection();
+
+            try
+            {
+                // DB 접속 정보
+                string dbConnectString = "Server=*;"
+                                       + "database=*;"
+                                       + "uid=yppos;"
+                                       + "pwd=*;";
+                // DB 접속 정보 입력
+                conn.ConnectionString = dbConnectString;
+                // DB 연결
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("DB 연결 오류 발생\r\n" + e);
+            }
+            // <--- DB 연결하기
+
+            SqlCommand sqlComm = new SqlCommand();
+
+            // 쿼리문
+            String SQL = string.Empty;
+
+            // 쿼리문 실행 결과 영향 받은 행의 수
+            int PTBOOKCLUBPOINT_NEW_NUM = 0;
+            int PTCARD_damo_NUM = 0;
+            int PTCASH_NEW_NUM = 0;
+            int PTCASHBILL_NEW_NUM = 0;
+            int PTCOUPON_NUM = 0;
+            int PTDETAIL_NUM = 0;
+            int PTLIB_NUM = 0;
+            int PTLIB_NEW_NUM = 0;
+            int PTLPOINT_NEW_NUM = 0;
+            int PTMCULTURELAND_NEW_NUM = 0;
+            int PTMHAPPYMONEY_NEW_NUM = 0;
+            int PTOCB_NUM = 0;
+            int PTSETTLE_NUM = 0;
+            int PTSTOCK_NUM = 0;
+
+            try
+            {
+                // PTBOOKCLUBPOINT_NEW 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTBOOKCLUBPOINT_NEW] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTBOOKCLUBPOINT_NEW_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTCARD_damo 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTCARD_damo] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTCARD_damo_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTCASH_NEW 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTCASH_NEW] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTCASH_NEW_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTCASHBILL_NEW 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTCASHBILL_NEW] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTCASHBILL_NEW_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTCOUPON 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTCOUPON] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTCOUPON_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTDETAIL 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTDETAIL] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTDETAIL_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTLIB 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTLIB] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTLIB_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTLIB_NEW 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTLIB_NEW] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTLIB_NEW_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTLPOINT_NEW 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTLPOINT_NEW] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTLPOINT_NEW_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTMCULTURELAND_NEW 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTMCULTURELAND_NEW] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTMCULTURELAND_NEW_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTMHAPPYMONEY_NEW 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTMHAPPYMONEY_NEW] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTMHAPPYMONEY_NEW_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTOCB 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTOCB] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTOCB_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTSETTLE 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTSETTLE] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTSETTLE_NUM = sqlComm.ExecuteNonQuery();
+
+                // PTSTOCK 테이블 삭제
+                SQL = "DELETE FROM *.[dbo].[PTSTOCK] WHERE BRCODE = @BRCODE AND WKDATE = @WKDATE AND POSNO = @POSNO AND GROUPNO = @GROUPNO AND VNO in (@VNO)";
+                sqlComm = new SqlCommand(SQL, conn);
+                sqlComm.Parameters.Add("@BRCODE", SqlDbType.Char).Value = _BRCODE;
+                sqlComm.Parameters.Add("@WKDATE", SqlDbType.Char).Value = _WKDATE;
+                sqlComm.Parameters.Add("@POSNO", SqlDbType.Int).Value = _POSNO;
+                sqlComm.Parameters.Add("@GROUPNO", SqlDbType.Int).Value = _GROUPNO;
+                sqlComm.Parameters.Add("@VNO", SqlDbType.Int).Value = _VNO;
+                PTSTOCK_NUM = sqlComm.ExecuteNonQuery();
+
+                // 메시지박스로 삭제 결과 출력
+                MessageBox.Show(string.Format("삭제 결과\r\n"
+                                            + "- PTBOOKCLUBPOINT_NEW 테이블 삭제 : {0} 건\r\n"
+                                            + "- PTCARD_damo 테이블 삭제 : {1} 건\r\n"
+                                            + "- PTCASH_NEW 테이블 삭제 : {2} 건\r\n"
+                                            + "- PTCASHBILL_NEW 테이블 삭제 : {3} 건\r\n"
+                                            + "- PTCOUPON 테이블 삭제 : {4} 건\r\n"
+                                            + "- PTDETAIL 테이블 삭제 : {5} 건\r\n"
+                                            + "- PTLIB 테이블 삭제 : {6} 건\r\n"
+                                            + "- PTLIB_NEW 테이블 삭제 : {7} 건\r\n"
+                                            + "- PTLPOINT_NEW 테이블 삭제 : {8} 건\r\n"
+                                            + "- PTMCULTURELAND_NEW 테이블 삭제 : {9} 건\r\n"
+                                            + "- PTMHAPPYMONEY_NEW 테이블 삭제 : {10} 건\r\n"
+                                            + "- PTOCB 테이블 삭제 : {11} 건\r\n"
+                                            + "- PTSETTLE 테이블 삭제 : {12} 건\r\n"
+                                            + "- PTSTOCK 테이블 삭제 : {13} 건",
+                                            PTBOOKCLUBPOINT_NEW_NUM, PTCARD_damo_NUM, PTCASH_NEW_NUM, PTCASHBILL_NEW_NUM, PTCOUPON_NUM, PTDETAIL_NUM, PTLIB_NUM,
+                                            PTLIB_NEW_NUM, PTLPOINT_NEW_NUM, PTMCULTURELAND_NEW_NUM, PTMHAPPYMONEY_NEW_NUM, PTOCB_NUM, PTSETTLE_NUM, PTSTOCK_NUM));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("SQL 쿼리 실행 중 오류 발생\r\n" + e);
+            }
+        }
+
+        private void btDELETE_Click(object sender, EventArgs e)
+        {
+            if (tbBRCODE.TextLength == 0 || tbWKDATE.TextLength == 0 || tbPOSNO.TextLength == 0 || tbGROUPNO.TextLength == 0 || tbVNO.TextLength == 0)
+            {
+                MessageBox.Show("조건값이 올바르지 않음");
+            }
+            else
+            {
+                DBDELETE(tbBRCODE.Text, tbWKDATE.Text, tbPOSNO.Text, tbGROUPNO.Text, tbVNO.Text);
+            }
+        }
+    }
+}
